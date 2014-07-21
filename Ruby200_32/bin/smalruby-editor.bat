067a8d85f59189352e2bc5c2eaa7e84fa4b3994e
Kouji Takao <kouji.takao@gmail.com>
2014-07-22 07:31:08 +0900

    assetsのコミット漏れを修正

--- Ruby200_32/bin/smalruby-editor.bat
+++ Ruby200_32/bin/smalruby-editor.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"ruby.exe" "C:/Users/kouji/work/smalruby/smalruby-installer-for-windows/Ruby200_32/bin/smalruby-editor" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"ruby.exe" "C:/smalruby-installer-for-windows/Ruby200_32/bin/smalruby-editor" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"ruby.exe" "%~dpn0" %*
