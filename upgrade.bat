32a8896c64f69a036a24e75e793d82c70e1ac2b8
Kouji Takao <kouji.takao@gmail.com>
2015-01-16 14:07:51 +0900

    added Windows batch file for upgrade smalruby-editor.

---
+++ upgrade.bat
@@ -0,0 +1,16 @@
+@setlocal
+
+@for /F "delims=" %%I in ("%~dp0DevKit200_32") do @set devkit_root=%%~fI
+@set PATH=%~dp0Ruby200_32\bin;%devkit_root%\bin;%devkit_root%\mingw\bin;%PATH%
+
+@if not exist "%HOME%" @set HOME=%HOMEDRIVE%%HOMEPATH%
+@if not exist "%HOME%" @set HOME=%USERPROFILE%
+
+@set PLINK_PROTOCOL=ssh
+@if not defined TERM set TERM=msys
+
+@cd /d %~dp0programs
+@set /P _=Upgrade (uninstall / install) smalruby-editor:
+call gem uninstall smalruby smalruby-editor
+call gem install smalruby-editor --no-ri --no-rdoc
+@set /P _=Press enter to quit:
